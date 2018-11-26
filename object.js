422b8fbeb8291a3c15d9a16fdac7ee0effb2042d
JD <jdwheeler0@gmail.com>
2018-11-26 14:49:47 -0600

    final in class work need to add functions

--- object.js
+++ object.js
@@ -1,10 +1,15 @@
+const hShot = document.getElementsByClassName('.candidate');
+const fShot = document.getElementsByClassName('.candidateFamily');
+const vShot = document.getElementsByClassName('.voters');
+
+
 const candidate = {
 
     candidateInfo: {
         name: "Elizabeth Sanger",
         nickname: "Betty",
         district: "Tennessee 5th Congressional District"
-    }
+    },
 
     candidateStance: {
 
@@ -14,11 +19,13 @@ const candidate = {
         healthCare: "Single Payer or bust",
         crime: "Let's stop turning people into criminals and disband the for profit prison system."
 
-    }
+    },
 
-    donationURL: www.giveMeYoMonies.ru;
+    donationURL: "www.giveMeYoMonies.ru",
 
-    eventsCalendar: [
+    eventsCalendar: 
+    {
+
 
         week1: {
             Sun: "Baby kissing at tattoo parlor!",
@@ -28,7 +35,7 @@ const candidate = {
             Thurs: "Off",
             Fri: "Gambling",
             Sat: "Running from the law: Full Marathon"
-        }
+        },
 
         week2: {
             Sun: "Opening new 'medical' dispensary",
@@ -38,7 +45,7 @@ const candidate = {
             Thurs: "Off",
             Fri: "Drinking",
             Sat: "Sleeping"
-        }
+        },
         week3: {
             Sun: "Off",
             Mon: "Off",
@@ -47,7 +54,7 @@ const candidate = {
             Thurs: "Off",
             Fri: "Drugs",
             Sat: "Off"
-        }
+        },
         week4: {
             Sun: "Partying at the retirement home!",
             Mon: "Off",
@@ -57,16 +64,114 @@ const candidate = {
             Fri: "Loving",
             Sat: "Chasing kids with my dogs at the park"
         }
-
-
-    ]
-
-
+    
+    },
     
 
+    volunteerDB: 
+    {
+
+        members: 
+        [
+            { 
+                name: "Joe Schmoe",
+                address: "12345 Fiction Lane Anywhere USA",
+                email: "jSchmoe@email.com",
+                phoneNumber: 555.5555,
+                activities: "Whatever needs to be done.",
+                availabilty: " Mon, Wed, Fri"
+
+            },
+            { 
+              name: "Jane Schmoe",
+                address: "12345 Fiction Lane Anywhere USA",
+                email: "jSchmoe@email.com",
+                phoneNumber: 555.5555,
+                activities: "Only the cool stuff.",
+                availabilty: " Mon, Wed, Fri"
+            },      
+              
+            {       
+                name: "Joe Jr Schmoe",
+                address: "12345 Fiction Lane Anywhere USA",
+                email: "jSchmoe@email.com",
+                phoneNumber: 555.5555,
+                activities: "Nothing really my mom is making me do this. ",
+                availabilty: " Mon "
+            }
+
+        ]
+
+    },
+
+    biography: "If you're working forty hours a week, you shouldn't be living in poverty. Instead, they expressed a profoundly distorted view of this country - a view that sees white racism as endemic, and that elevates what is wrong with America above all that we know is right with America; a view that sees the conflicts in the Middle East as rooted primarily in the actions of stalwart allies like Israel, instead of emanating from the perverse and hateful ideologies of radical Islam.",
+
+   
+
+    images:{
+
+        headshot: hShot,
+        familyShot: fShot,
+        voters: vShot
+    },
+
+    missonStatment:"Drawing on her experience successfully taking on the financial industry even in the face of fierce opposition from Wall Street and big corporations, Elizabeth has effectively fought to expose big banks, fraudulent lenders, and greedy for-profit colleges that have preyed on borrowers, students, members of the armed services, and taxpayers.  Her work has resulted in real accountability for executives at Wells Fargo who conspired to defraud customers, changed rules to help prevent taxpayer bailouts of Wall Street risk-takers, led to new laws to protect students from being ripped off by predatory institutions, and helped tens of thousands of people get their money back when they were scammed.",
 
 
-
+    voterURL: "https://ovr.govote.tn.gov/Registration/RegistrationDetails/BM"
 
 
 }
+
+
+console.log(candidate.missonStatment);
+
+
+
+// function updateInfo (){
+
+//     In this area you will be able to update the info Object.
+
+// }
+
+// function updateStance(){
+
+//   In this area you will be able to update the info Object.
+
+// }
+
+// function updateDonation(){
+
+//     Update the donate URL.
+
+// }
+
+// function updateEventsCalendar(){
+
+//     Updates to updateEventsCalendar.
+// }
+
+// function updateVolunteerDB (){
+
+//     Updates to the volunteer database.
+// }
+
+// function updateBiography (){
+
+//     update existing biography.
+// }
+
+// function updateImages(){
+
+
+// }
+
+// function updateMission() {
+
+
+// }
+
+// function updateVoteURL (){
+
+
+// }
